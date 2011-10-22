a35f2d34edf86228ce102acd599b5bd57de92198
Patrick Hanevold <patrick.hanevold@gmail.com>
2011-10-22 18:33:57 +0200

    linguified

--- lib/morel.rb
+++ lib/morel.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
 require 'mongo'
+require 'linguify'
 require 'morel/collection'
 require 'morel/sorted_window'
--- lib/morel/sorted_window.rb
+++ lib/morel/sorted_window.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 
-require 'goling'
-
 module Morel
 
   class Collection
